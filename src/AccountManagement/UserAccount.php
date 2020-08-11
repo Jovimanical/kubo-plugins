cbd86bbe693dbd483bdcd1e147029ca260032b02
samshal <samueladeshina73@gmail.com>
2020-08-11 08:36:24 +0100

    Another test module

---
+++ src/AccountManagement/UserAccount.php
@@ -0,0 +1,13 @@
+<?php declare (strict_types=1);
+
+Namespace KuboPlugin\AccountManagement;
+
+class UserAccount {
+
+	public function displayUsers(){
+		return [
+			"James", "Elo", "Message", "Samuel"
+		];
+	}
+	
+}

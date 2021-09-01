819ce26d82f0b6ba0f9929883eb619507b94ac09
samshal <samueladeshina73@gmail.com>
2021-09-01 14:47:10 +0100

    Intersect children metadata with parents

--- src/Properties/UserProperty/UserProperty.php
+++ src/Properties/UserProperty/UserProperty.php
@@ -157,15 +157,28 @@ class UserProperty {
 
         $result = DBConnectionFactory::getConnection()->query($query)->fetchAll(\PDO::FETCH_ASSOC);
 
+        $propertyParentQuery = "SELECT MetadataId, FieldName, FieldValue FROM Properties.UserPropertyMetadata
+                                WHERE PropertyId = (SELECT PropertyId FROM Properties.UserProperty WHERE LinkedEntity = (SELECT b.EntityParent FROM Properties.UserProperty a INNER JOIN 
+                                SpatialEntities.Entities b ON a.LinkedEntity = b.EntityId WHERE PropertyId=$parentId))";
+        $propertyParentResult = DBConnectionFactory::getConnection()->query($propertyParentQuery)->fetchAll(\PDO::FETCH_ASSOC);
+
         $metadata = [];
 
         foreach ($result as $key=>$value){
             if (!isset($metadata[$value["PropertyId"]])){
+                $propertyId = $value["PropertyId"];
                 $metadata[$value["PropertyId"]] = [];
             }
 
             $metadata[$value["PropertyId"]][$value["FieldName"]] = ["FieldValue"=>$value["FieldValue"], "MetadataId"=>$value["MetadataId"]];
+
+            foreach ($propertyParentResult as $key => $value){
+                if (!isset($metadata[$propertyId][$value["FieldName"]])){
+                    $metadata[$propertyId][$value["FieldName"]] = ["FieldValue"=>$value["FieldValue"], "MetadataId"=>$value["MetadataId"]];
+                }
+            }
         }
+
 
         return $metadata;
     }

351cf4c3b541cefd200e9c3bc3c20529fae63195
jigga.e10@gmail.com <Wulfdog@7>
2021-10-12 12:43:43 +0100

    fix array_push error on Enquiry API

--- src/Properties/UserEnquiry/Enquiry.php
+++ src/Properties/UserEnquiry/Enquiry.php
@@ -70,7 +70,7 @@ class Enquiry {
         $query = "SELECT * FROM Properties.Enquiries ORDER BY EnquiryId DESC OFFSET $offset ROWS FETCH $fetch 5 ROWS ONLY";  // EnquiryId = $EnquiryId
         $result = DBConnectionFactory::getConnection()->query($query)->fetchAll(\PDO::FETCH_ASSOC);
 
-        $resultArr = "";
+        $resultArr = [];
         foreach($result as $resultum){
             $resultMsg = $resultum['MessageJson'];
             $resultum['MessageJsonX'] = unserialize($resultMsg);

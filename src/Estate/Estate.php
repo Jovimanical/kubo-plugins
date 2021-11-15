48bfd398303901ee8f9da71c58c981c32b46aa2e
jigga.e10@gmail.com <Wulfdog@7>
2021-11-15 17:34:42 +0100

    updated the landing class for Estate namespace

--- src/Estate/Estate.php
+++ src/Estate/Estate.php
@@ -21,8 +21,8 @@ namespace KuboPlugin\Estate;
  * @since v0.0.1 18/10/2021 06:20
  */
 class Estate {
-	public static function updateEstateUser(array $data){
-		return Estate\Estate::updateEstateUser($data);
+	public static function updateEstateUser(int $userId,array $data){
+		return Estate\Estate::updateEstateUser($userId,$data);
 	}
 
     public static function viewEstateUser(int $userId,array $data){

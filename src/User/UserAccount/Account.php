6e5f9f28638197eb51f44694de74dcac4c808001
jigga.e10@gmail.com <Wulfdog@7>
2021-11-17 17:28:54 +0100

    fixed changepassword undefined index bug test commit

--- src/User/UserAccount/Account.php
+++ src/User/UserAccount/Account.php
@@ -136,6 +136,8 @@ class Account {
 
         $data_new_password = $data['new_password'];
 
+        return $result;
+
         if($passwordHash == $result['PasswordHash']){
             $query1 = "UPDATE Users.Account SET PasswordHash = '$data_new_password', WHERE UserId = '$resourceId'";
 			$result1 = DBConnectionFactory::getConnection()->exec($query1);

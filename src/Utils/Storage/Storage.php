339ebbc3602090676f393cc311472395436c9f4a
jigga.e10@gmail.com <Wulfdog@7>
2021-11-29 17:42:49 +0100

    1201438755889678-modifying-and-ensuring-that-editPropertyMetadata-API-endpoint-can-handle-and-upload-images-into-the-file-system and testing json_encoded array data while removing var_dump() tester1

--- src/Utils/Storage/Storage.php
+++ src/Utils/Storage/Storage.php
@@ -1,4 +1,4 @@
-<?php declare (strict_types=1);
+<?php declare (strict_types = 1);
 /**
  * Controller Class.
  *
@@ -12,10 +12,6 @@
 
 namespace KuboPlugin\Utils\Storage;
 
-use EmmetBlue\Core\Factory\DatabaseConnectionFactory as DBConnectionFactory;
-use EmmetBlue\Core\Factory\DatabaseQueryFactory as DBQueryFactory;
-use EmmetBlue\Core\Builder\QueryBuilder\QueryBuilder as QB;
-
 /**
  * class KuboPlugin\Utils\Storage
  *
@@ -24,72 +20,68 @@ use EmmetBlue\Core\Builder\QueryBuilder\QueryBuilder as QB;
  * @author Samuel Adeshina <samueladeshina73@gmail.com>
  * @since v0.0.1 28/11/2021 11:57
  */
-class Storage {
-    public static function storeBase64(array $data){
+class Storage
+{
+    public static function storeBase64(array $data)
+    {
         try {
 
-        
-        //Save a base64 string in solution storage bucket and return an array [status: true, ref: unique_ref] to the saved object.
-        $object = $data["object"] ?? "";
+            //Save a base64 string in solution storage bucket and return an array [status: true, ref: unique_ref] to the saved object.
+            $object = $data["object"] ?? "";
 
-        if(empty($object)){
-            return [
-                "status" => false,
-                "message" => "Object is required"
-            ];
-        }
+            if (empty($object)) {
+                return [
+                    "status" => false,
+                    "message" => "Object is required",
+                ];
+            }
 
-        $fileName = time()."_".uniqid().".png";
-        $filePath = "/var/www/html/kubo-core/";
+            $fileName = time() . "_" . uniqid() . ".png";
+            $filePath = "/var/www/html/kubo-core/";
 
-        $ref = self::base64ToImg($object,$filePath,$fileName);
+            $ref = self::base64ToImg($object, $filePath, $fileName);
 
-        if(!$ref){
-            return [
-                "status" => false,
-                "message" => "Failed to save image"
-            ];
-        }
+            if (!$ref) {
+                return [
+                    "status" => false,
+                    "message" => "Failed to save image",
+                ];
+            }
 
-       // var_dump($ref);
+            // var_dump($ref);
 
-        return ["status"=>true, "ref"=>$ref];
+            return ["status" => true, "ref" => $ref];
 
-        } catch(\Exception $e){
-          return ["status"=>false, "message"=>$e->getMessage()];
+        } catch (\Exception $e) {
+            return ["status" => false, "message" => $e->getMessage()];
         }
 
     }
-    
-    }
 
-    public static function base64ToImg($base64String, $filePath, $outputFile) {
+    public static function base64ToImg($base64String, $filePath, $outputFile)
+    {
         // check for dir
-        $filer = $filePath."uploads/".$outputFile;
+        $filer = $filePath . "uploads/" . $outputFile;
 
         $dirname = dirname($filer);
-        if (!is_dir($dirname))
-        {
+        if (!is_dir($dirname)) {
             mkdir($dirname, 0755, true);
         }
         // open file for writing
-        $imgStringFile = fopen( $filePath."uploads/".$outputFile, 'w' );
-
-        
+        $imgStringFile = fopen($filePath . "uploads/" . $outputFile, 'w');
 
         // split the string on commas
-        $dataImg = explode( ',', $base64String );
+        $dataImg = explode(',', $base64String);
 
-        $writer = fwrite( $imgStringFile, base64_decode( $dataImg[1] ) );
-
+        $writer = fwrite($imgStringFile, base64_decode($dataImg[1]));
 
         // clean up the file resource
         fclose($imgStringFile);
 
-        if(!$writer){
+        if (!$writer) {
             return false;
         }
 
-        return "uploads/".$outputFile;
+        return "uploads/" . $outputFile;
     }
 }

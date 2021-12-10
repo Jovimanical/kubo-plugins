3dd82f7ac28cf3c95e5c19877be74ed4bec5fb3f
jigga.e10@gmail.com <Wulfdog@7>
2021-12-10 10:51:03 +0100

    added company related data field name types to view user info api test 5

--- src/User/UserInfo/UserInfo.php
+++ src/User/UserInfo/UserInfo.php
@@ -123,11 +123,16 @@ class UserInfo
         }
         
         $query = "SELECT UserId,FirstName,LastName,ProfilePhotoUrl,PhoneNumber FROM Users.UserInfo WHERE UserId=$userId";
-        $result = DBConnectionFactory::getConnection()->query($query)->fetchAll(\PDO::FETCH_ASSOC);
+        $result['data'] = DBConnectionFactory::getConnection()->query($query)->fetchAll(\PDO::FETCH_ASSOC);
         
         $queryMeta = "SELECT FieldValue,FieldName FROM [Users].[UserInfoFieldValues] LEFT JOIN [Users].[UserInfoFields] ON [Users].[UserInfoFieldValues].FieldId = [Users].[UserInfoFields].FieldId WHERE UserId = $userId;";
         $result['meta'] = DBConnectionFactory::getConnection()->query($queryMeta)->fetchAll(\PDO::FETCH_ASSOC);
+         
+        foreach($result['meta'] as $key => $value){
 
+            $result['data'][$key]  =  $value;
+        }
+        
         if ($result) {
             return $result;
         } else {

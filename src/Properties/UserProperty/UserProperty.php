9fd35c03a4b63be62d55a080b8ee3e62370d19b7
jigga.e10@gmail.com <Wulfdog@7>
2021-12-20 18:17:28 +0100

    added a fix for getEstateAvailable api

--- src/Properties/UserProperty/UserProperty.php
+++ src/Properties/UserProperty/UserProperty.php
@@ -427,7 +427,7 @@ class UserProperty
         $query = "SELECT a.* FROM Properties.UserPropertyMetadata a
         INNER JOIN Properties.UserProperty b ON a.PropertyId = b.PropertyId
         INNER JOIN SpatialEntities.Entities c ON b.LinkedEntity = c.EntityId
-        WHERE c.EntityParent IN($resultString) AND a.FieldName = 'property_status' AND a.FieldValue = 'false'";
+        WHERE a.FieldName = 'property_status' AND a.FieldValue = 'false' AND b.LinkedEntity IN($resultString)";
 
         $resultTwos = DBConnectionFactory::getConnection()->query($query)->fetchAll(\PDO::FETCH_ASSOC);
 

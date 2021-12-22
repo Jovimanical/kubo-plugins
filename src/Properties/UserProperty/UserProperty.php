a16db1fd946f2306e4b5ab65c0718b0f3fbfecaf
jigga.e10@gmail.com <Wulfdog@7>
2021-12-22 11:18:05 +0100

    modified getEstatePropertyavailable endpoints with '' for 'false' in sql query for testing 1

--- src/Properties/UserProperty/UserProperty.php
+++ src/Properties/UserProperty/UserProperty.php
@@ -427,14 +427,14 @@ class UserProperty
         $query = "SELECT a.* FROM Properties.UserPropertyMetadata a
         INNER JOIN Properties.UserProperty b ON a.PropertyId = b.PropertyId
         INNER JOIN SpatialEntities.Entities c ON b.LinkedEntity = c.EntityId
-        WHERE a.FieldName = 'property_status' AND a.FieldValue = '' AND c.EntityParent IN($resultString)";
+        WHERE a.FieldName = 'property_status' AND a.FieldValue != '' AND c.EntityParent IN($resultString)";
 
         $resultTwos = DBConnectionFactory::getConnection()->query($query)->fetchAll(\PDO::FETCH_ASSOC);
 
         $propertyCount = count($resultTwos);
-        //return ((int)$propertyTotal - (int)$propertyCount);
+        return ((int)$propertyTotal - (int)$propertyCount);
 
-        return $propertyCount;
+        //return $propertyCount;
 
     }
 

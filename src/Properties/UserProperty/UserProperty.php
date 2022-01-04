8c26bc546006602422f5ecd236ad31662f8a110a
jigga.e10@gmail.com <Wulfdog@7>
2022-01-04 19:06:11 +0100

    added an intersection for property attributes found on estate level and not found on property unit level test 43

--- src/Properties/UserProperty/UserProperty.php
+++ src/Properties/UserProperty/UserProperty.php
@@ -225,7 +225,7 @@ class UserProperty
         //var_dump($blockResult);
 
         foreach ($blockResult as $keyItem => $valueItem) {
-            if (empty($metadata[$valueItem["FieldValue"]]) OR $metadata[$valueItem["FieldValue"]] == "[]") {
+            if (isset($metadata[$valueItem["FieldValue"]]) AND empty($metadata[$valueItem["FieldValue"]]) OR isset($metadata[$valueItem["FieldValue"]]) AND $metadata[$valueItem["FieldValue"]] == "[]" OR !isset($metadata[$valueItem["FieldValue"]])) {
                 $metadata[$valueItem["FieldName"]] = ["FieldValue" => $valueItem["FieldValue"], "MetadataId" => $valueItem["MetadataId"]];
             }
         }

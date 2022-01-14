3a0e15676b83bd851d35d0d22e4d8137dac822f9
jigga.e10@gmail.com <Wulfdog@7>
2022-01-14 04:50:11 +0100

    testing the view user info endpoint 16

--- src/Properties/UserProperty/UserProperty.php
+++ src/Properties/UserProperty/UserProperty.php
@@ -353,7 +353,7 @@ class UserProperty
             $queries[] = "BEGIN TRANSACTION;" .
                 "DECLARE @rowcount INT;".
                 "UPDATE Properties.UserPropertyMetadata SET FieldValue='$value' WHERE FieldName='$keyId' AND PropertyId=$propertyId " .
-                "SET @rowcount = @@ROWCOUNT" .
+                "SET @rowcount = @@ROWCOUNT;" .
                 "BEGIN TRY " .
                 "IF @rowcount = 0 BEGIN INSERT INTO Properties.UserPropertyMetadata (PropertyId, FieldName, FieldValue) VALUES ($propertyId, '$keyId', '$value') END;" .
                 "END TRY BEGIN CATCH SELECT ERROR_NUMBER() AS ErrorNumber,ERROR_MESSAGE() AS ErrorMessage; END CATCH " .

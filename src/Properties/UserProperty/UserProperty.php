dbe538fbf3c2158e3c4b3c25eeb1b78cb85d0060
jigga.e10@gmail.com <Wulfdog@7>
2022-01-24 14:11:03 +0100

    added feature to block edit to edit all block property children for first edits testing 41

--- src/Properties/UserProperty/UserProperty.php
+++ src/Properties/UserProperty/UserProperty.php
@@ -368,15 +368,16 @@ class UserProperty
         if (count($blockChildrenIds) > 0) {
             $initialQuery = "SELECT Initial FROM Properties.UserProperty WHERE PropertyId = $propertyId";
             $resultInitial = DBConnectionFactory::getConnection()->query($initialQuery)->fetchAll(\PDO::FETCH_ASSOC);
-            die(var_dump($resultInitial));
+            
             if(isset($resultInitial["Initial"]) AND is_null($resultInitial["Initial"])) {
                 $initialCheck = true;
+                die(var_dump($resultInitial));
 
                 $queries[] = "BEGIN TRANSACTION;" .
                              "UPDATE Properties.UserProperty SET Initial='true' WHERE PropertyId=$propertyId;" .
                              "COMMIT TRANSACTION;";
             }
-            die(var_dump($queries));
+            
 
         }
 

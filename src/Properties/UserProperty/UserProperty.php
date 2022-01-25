ed4a3150e6ed1d6a31d4c5e191e4dfb70e56f089
jigga.e10@gmail.com <Wulfdog@7>
2022-01-25 15:59:49 +0100

    added feature to block edit to edit all block property children for first edits testing 87

--- src/Properties/UserProperty/UserProperty.php
+++ src/Properties/UserProperty/UserProperty.php
@@ -272,13 +272,13 @@ class UserProperty
 
             }
         } while ($stmtBlock->nextRowset());
-        die(var_dump($unitResultSetArr));
+         // die(var_dump($unitResultSetArr));
         foreach ($results as $keySetId => $valueSetId) {
 
             foreach ($unitResultSetArr as $keySet => $valueSet) {
                 foreach ($valueSet as $keyItemId => $valueItemId) {
 
-                        if ((int)$valueSetId['PropertyId'] === (int)$valueItemId['PropertyId']) {
+                        if ($valueSetId['PropertyId'] == $valueItemId['PropertyId']) {
                             $metadata[$valueItemId["FieldName"]] = ["FieldValue" => $valueItemId["FieldValue"], "MetadataId" => $valueItemId["MetadataId"], "PropertyId" => $valueItemId["PropertyId"]];
                         }
 

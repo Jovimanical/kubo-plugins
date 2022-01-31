be56540a9530ee4a419027c82596dac4ac07ec6b
jigga.e10@gmail.com <Wulfdog@7>
2022-01-31 16:34:56 +0100

    testing newPropertyOnEntity 14

--- src/Properties/UserEnquiry/Enquiry.php
+++ src/Properties/UserEnquiry/Enquiry.php
@@ -1074,6 +1074,7 @@ class Enquiry {
             $resultum['MessageJsonX'] = str_replace("&#39;","'",unserialize($resultMsg));
            // $result["PropertyData"] = UserProperty::viewProperty((int)$result["PropertyId"]);
             array_push($resultArr,$resultum);
+            
 
         }
 

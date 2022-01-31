b6028a8798c59b8cb72006537ad271d63fc2b8a5
jigga.e10@gmail.com <Wulfdog@7>
2022-01-31 15:26:15 +0100

    testing newPropertyOnEntity 11

--- src/Properties/UserProperty/UserProperty.php
+++ src/Properties/UserProperty/UserProperty.php
@@ -106,7 +106,7 @@ class UserProperty
             $propId = $result["lastInsertId"];
         }
 
-        if (self::isJSON($metadata)) {
+        if (self::isJSONExtra($metadata)) {
             $metadata = str_replace('&#39;', '\'', $metadata);
             $metadata = str_replace('&#34;', '"', $metadata);
             $metadata = json_decode($metadata, true);
@@ -794,6 +794,13 @@ class UserProperty
     protected static function isJSON($stringData)
     {
         $string = str_replace('&#39;', '"', $stringData);
+        $string = str_replace('&#34;', '"', $stringData);
+        return is_string($string) && is_array(json_decode($string, true)) ? true : false;
+    }
+
+    protected static function isJSONExtra($stringData)
+    {
+        $string = str_replace('&#39;', '\'', $stringData);
         $string = str_replace('&#34;', '"', $stringData);
         return is_string($string) && is_array(json_decode($string, true)) ? true : false;
     }

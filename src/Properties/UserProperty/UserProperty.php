50f4e9102b631db9ce979736faf6b64a03ec4d29
jigga.e10@gmail.com <jigga.e10@gmail.com>
2022-04-28 13:09:08 +0100

    bug fix for add floor

--- src/Properties/UserProperty/UserProperty.php
+++ src/Properties/UserProperty/UserProperty.php
@@ -3720,8 +3720,6 @@ class UserProperty
 
             $queryInsertBlocks = implode(";", $queries);
 
-            return $queryInsertBlocks;
-
             $resultSet = DBConnectionFactory::getConnection()->exec($queryInsertBlocks);
 
             // returning block data array
